902079f39bc1c5391130670016be2b1cd355607e
Pedro Sousa <pedronsousa@gmail.com>
2020-08-17 20:10:21 +0100

    Normalize address in Memory

--- Z80a/lib/Memory.dart
+++ Z80a/lib/Memory.dart
@@ -9,16 +9,19 @@ class Memory {
     this.bytes = List<int>.filled(size, 0);
   }
 
-  peek(int address) => bytes[address];
+  int normalize(int address) => address % 65536;
 
-  peek2(int address) => bytes[address] + 256 * bytes[address + 1];
+  peek(int address) => bytes[normalize(address)];
+
+  peek2(int address) =>
+      bytes[normalize(address)] + 256 * bytes[normalize(address + 1)];
 
   poke(int address, int b) {
-    this.bytes[address] = b;
+    this.bytes[normalize(address)] = b;
   }
 
   poke2(int address, int b) {
-    this.bytes[address] = lo(b);
-    this.bytes[address + 1] = hi(b);
+    this.bytes[normalize(address)] = lo(b);
+    this.bytes[normalize(address + 1)] = hi(b);
   }
 }
