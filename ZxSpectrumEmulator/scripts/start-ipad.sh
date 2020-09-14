b31fcc1a3645cbecae4dceb56975161347e2f152
Pedro Sousa <pedronsousa@gmail.com>
2020-09-14 17:41:20 +0100

    Add simulator scripts

---
+++ ZxSpectrumEmulator/scripts/start-ipad.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+open -a Simulator --args --CurrentDeviceUDID 7010FA72-3BFE-417C-8160-CC6B3640C489
---
+++ ZxSpectrumEmulator/scripts/start-iphone.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+open -a Simulator --args --CurrentDeviceUDID CC9808FE-A2B3-4555-A931-2B3E2211AA51
